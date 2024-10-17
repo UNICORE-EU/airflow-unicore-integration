e46c0e50a7b66eeee1902129c95114e899e17761
Christian <c.boettcher@fz-juelich.de>
2024-10-17 13:58:14 +0200

    dont overwrite a shared connection with details from a single task, just create a new one every time

--- src/airflow_unicore_integration/hooks/unicore_hooks.py
+++ src/airflow_unicore_integration/hooks/unicore_hooks.py
@@ -22,21 +22,18 @@ class UnicoreHook(BaseHook):
     def __init__(self, uc_conn_id: str = default_conn_name) -> None:
         super().__init__()
         self.uc_conn_id = uc_conn_id
-        self.conn = None
 
     def get_conn(self, overwrite_base_url: str | None = None, overwrite_credential : credentials.Credential | None = None) -> client.Client:
         """Return a Unicore Client. base_url and credentials may be overwritten."""
-        if self.conn is None or overwrite_base_url is not None or overwrite_credential is not None: # if not generated, or overwrite attribute is set crete new
-            params = self.get_connection(self.uc_conn_id)
-            base_url = params.host
-            credential = credentials.UsernamePassword(params.login, params.password)
-            if overwrite_base_url is not None:
-                base_url = overwrite_base_url
-            if overwrite_credential is not None:
-                credential = overwrite_credential
-            self.conn = client.Client(credential, base_url)
-
-        return self.conn
+        params = self.get_connection(self.uc_conn_id)
+        base_url = params.host
+        credential = credentials.UsernamePassword(params.login, params.password)
+        if overwrite_base_url is not None:
+            base_url = overwrite_base_url
+        if overwrite_credential is not None:
+            credential = overwrite_credential
+        conn = client.Client(credential, base_url)
+        return conn
 
 
     def test_connection(self) -> tuple[bool, str]:
