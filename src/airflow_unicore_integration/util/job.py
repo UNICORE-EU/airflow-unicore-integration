c11e017ccd91fdacbc0ba8852f8a4765b427dee5
Christian Boettcher <c.boettcher@fz-juelich.de>
2025-09-25 11:32:29 +0200

    escape the env vars to avoid trouble with unicore interpreting them wrong

--- src/airflow_unicore_integration/util/job.py
+++ src/airflow_unicore_integration/util/job.py
@@ -1,3 +1,4 @@
+import json
 import os
 from typing import Any
 from typing import Dict
@@ -84,14 +85,16 @@ class NaiveJobDescriptionGenerator(JobDescriptionGenerator):
             "AIRFLOW__CORE__DAGS_FOLDER": "./",
             "AIRFLOW__LOGGING__LOGGING_LEVEL": "DEBUG",
             "AIRFLOW__CORE__EXECUTOR": "LocalExecutor,airflow_unicore_integration.executors.unicore_executor.UnicoreExecutor",
-            "AIRFLOW__DAG_PROCESSOR__DAG_BUNDLE_CONFIG_LIST": os.environ.get(
-                "AIRFLOW__DAG_PROCESSOR__DAG_BUNDLE_CONFIG_LIST", ""
-            ),
+            "AIRFLOW__DAG_PROCESSOR__DAG_BUNDLE_CONFIG_LIST": json.dumps(
+                os.environ.get("AIRFLOW__DAG_PROCESSOR__DAG_BUNDLE_CONFIG_LIST", "")
+            ).replace("\\n", ""),
         }
         # insert connection details that are provided via env vars to get bundles
         for env_key in os.environ.keys():
             if env_key.startswith("AIRFLOW_CONN_"):
-                job_descr_dict["Environment"][env_key] = os.environ[env_key]
+                job_descr_dict["Environment"][env_key] = json.dumps(os.environ[env_key]).replace(
+                    "\\n", ""
+                )
         user_added_pre_commands.append(f"source {python_env}/bin/activate")
         job_descr_dict["User precommand"] = ";".join(user_added_pre_commands)
         job_descr_dict["RunUserPrecommandOnLoginNode"] = (
