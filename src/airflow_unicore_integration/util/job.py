452ed3ddf9201c577bd695512fa8b6f1918d02ce
Christian Boettcher <c.boettcher@fz-juelich.de>
2025-11-13 15:11:45 +0100

    automatically clone and configure dagbundles via git

--- src/airflow_unicore_integration/util/job.py
+++ src/airflow_unicore_integration/util/job.py
@@ -1,10 +1,16 @@
-import os
+import json
+import logging
 from typing import Any
 from typing import Dict
 
 from airflow.configuration import conf
 from airflow.executors.workloads import ExecuteTask
 from airflow.models.taskinstancekey import TaskInstanceKey
+from airflow.providers.git.hooks.git import GitHook
+
+from .launch_script_content import LAUNCH_SCRIPT_CONTENT_STR
+
+logger = logging.getLogger(__name__)
 
 
 class JobDescriptionGenerator:
@@ -18,6 +24,7 @@ class JobDescriptionGenerator:
     EXECUTOR_CONFIG_PARAMETERS = "Parameters"  # gets added to the unicore job description
     EXECUTOR_CONFIG_PROJECT = "Project"  # gets added to the unicore job description
     EXECUTOR_CONFIG_PRE_COMMANDS = "precommands"  # gets added to the unicore job description
+    EXECUTOR_CONFIG_POST_COMMANDS = "postcommands"  # gets added to the unicore job descirption
     EXECUTOR_CONFIG_UNICORE_CONN_KEY = (
         "unicore_connection_id"  # alternative connection id for the Unicore connection to use
     )
@@ -33,6 +40,8 @@ class NaiveJobDescriptionGenerator(JobDescriptionGenerator):
     This class generates a naive unicore job, that expects there to be a working python env containign airflow and any other required dependencies on the executing system.
     """
 
+    GIT_DAG_BUNDLE_CLASSPATH = "airflow.providers.git.bundles.git.GitDagBundle"
+
     def create_job_description(self, workload: ExecuteTask) -> Dict[str, Any]:
         key: TaskInstanceKey = workload.ti.key
         executor_config = workload.ti.executor_config
@@ -46,6 +55,7 @@ class NaiveJobDescriptionGenerator(JobDescriptionGenerator):
         user_added_resources: Dict[str, str] = executor_config.get(JobDescriptionGenerator.EXECUTOR_CONFIG_RESOURCES, None)  # type: ignore
         user_added_pre_commands: list[str] = executor_config.get(JobDescriptionGenerator.EXECUTOR_CONFIG_PRE_COMMANDS, [])  # type: ignore
         user_defined_python_env: str = workload.ti.executor_config.get(JobDescriptionGenerator.EXECUTOR_CONFIG_PYTHON_ENV_KEY, None)  # type: ignore
+        user_added_post_commands: list[str] = executor_config.get(JobDescriptionGenerator.EXECUTOR_CONFIG_POST_COMMANDS, [])  # type: ignore
         # get local dag path from cmd and fix dag path in arguments
         dag_rel_path = str(workload.dag_rel_path)
         if dag_rel_path.startswith("DAG_FOLDER"):
@@ -56,54 +66,97 @@ class NaiveJobDescriptionGenerator(JobDescriptionGenerator):
         server = conf.get(
             "unicore.executor", "execution_api_server_url", fallback=default_execution_api_server
         )
+        logger.debug(f"Server is {server}")
 
         # check which python virtualenv to use
         if user_defined_python_env:
             python_env = user_defined_python_env
         else:
             python_env = conf.get("unicore.executor", "DEFAULT_ENV")
+        tmp_dir = conf.get("unicore.executor", "TMP_DIR", "/tmp")
         # prepare dag file to be uploaded via unicore
         # dag_file = open("/tmp/test")
         # dag_content = dag_file.readlines()
         # dag_import = {"To": dag_rel_path, "Data": dag_content}
         worker_script_import = {
             "To": "run_task_via_supervisor.py",
-            "From": "https://gist.githubusercontent.com/cboettcher/3f1101a1d1b67e7944d17c02ecd69930/raw/1d90bf38199d8c0adf47a79c8840c3e3ddf57462/run_task_via_supervisor.py",
+            # "From": "https://gist.githubusercontent.com/cboettcher/3f1101a1d1b67e7944d17c02ecd69930/raw/1d90bf38199d8c0adf47a79c8840c3e3ddf57462/run_task_via_supervisor.py",
+            "Data": LAUNCH_SCRIPT_CONTENT_STR,
         }
         # start filling the actual job description
         job_descr_dict["Name"] = f"{key.dag_id} - {key.task_id} - {key.run_id} - {key.try_number}"
         job_descr_dict["Executable"] = (
-            "python"  # TODO may require module load to be setup for some systems
+            f". airflow_config.env && . {python_env} && python run_task_via_supervisor.py --json-string '{workload.model_dump_json()}'"  # TODO may require module load to be setup for some systems
         )
-        job_descr_dict["Arguments"] = [
-            "run_task_via_supervisor.py",
-            f"--json-string '{workload.model_dump_json()}'",
-        ]
+        # job_descr_dict["Arguments"] = [
+        #    "-c",
+        #    "source airflow_config.env",
+        #    "source {python_env}/bin/activate",
+        #    "python",
+        #    "run_task_via_supervisor.py",
+        #    f"--json-string '{workload.model_dump_json()}'",
+        # ]
+
         job_descr_dict["Environment"] = {
             "AIRFLOW__CORE__EXECUTION_API_SERVER_URL": server,
-            "AIRFLOW__CORE__DAGS_FOLDER": "./",
+            # "AIRFLOW__CORE__DAGS_FOLDER": "./",
             "AIRFLOW__LOGGING__LOGGING_LEVEL": "DEBUG",
             "AIRFLOW__CORE__EXECUTOR": "LocalExecutor,airflow_unicore_integration.executors.unicore_executor.UnicoreExecutor",
+            "AIRFLOW__DAG_PROCESSOR__DAG_BUNDLE_STORAGE_PATH": f"{tmp_dir}/{workload.ti.id}/dagbundle",
         }
 
         # build filecontent string for importing in the job | this is needed to avoid confusing nested quotes and trying to escape them properly when using unicore env vars directly
-        env_file_content: list[str] = [
-            f"export AIRFLOW__DAG_PROCESSOR__DAG_BUNDLE_CONFIG_LIST='{os.environ.get("AIRFLOW__DAG_PROCESSOR__DAG_BUNDLE_CONFIG_LIST", "")}'"
-        ]
+        env_file_content: list[str] = []
 
-        # insert connection details that are provided via env vars to get bundles
-        for env_key in os.environ.keys():
-            if env_key.startswith("AIRFLOW_CONN_"):
-                env_file_content.append(f"export {env_key}='{os.environ[env_key]}'")
+        # transmit needed dag bundle information (and possibly files) to job directory
+        bundle_str = conf.get("dag.processor", "dag_bundle_config_list")
+        logger.debug(f"Dag Bundle config is: {bundle_str}")
+        bundle_dict = json.loads(bundle_str)
+        conn_id_to_transmit = None
+        bundle_type = None
+
+        for bundle in bundle_dict:
+            if bundle["name"] == workload.bundle_info.name:
+                if bundle["classpath"] == NaiveJobDescriptionGenerator.GIT_DAG_BUNDLE_CLASSPATH:
+                    bundle_type = NaiveJobDescriptionGenerator.GIT_DAG_BUNDLE_CLASSPATH
+                    env_file_content.append(
+                        f"export AIRFLOW__DAG_PROCESSOR__DAG_BUNDLE_CONFIG_LIST='[{json.dumps(bundle)}]'"
+                    )
+                    conn_id_to_transmit = bundle["kwargs"]["git_conn_id"]
+                    break
+                # TODO handle other bundle types
+
+        if bundle_type:
+            if (
+                bundle_type == NaiveJobDescriptionGenerator.GIT_DAG_BUNDLE_CLASSPATH
+                and conn_id_to_transmit
+            ):
+                git_hook = GitHook(conn_id_to_transmit)
+                git_remote_url = git_hook.repo_url
+                git_local_url = f"{tmp_dir}/{workload.ti.id}/dagmirror"
+                # add precommand to clone repo on ligon node
+                git_precommand = f". {python_env} && mkdir -p {tmp_dir}/{workload.ti.id}/dagmirror && mkdir -p {tmp_dir}/{workload.ti.id}/dagbundle && git clone {git_remote_url} {git_local_url}"
+                logger.info(f"git precommand is {git_precommand}")
+                user_added_pre_commands.append(git_precommand)
+                # add connection to local clone to env of job
+                airflow_conn_string = json.dumps(
+                    {"conn_type": "git", "host": f"file://{git_local_url}"}
+                )
+                env_file_content.append(
+                    f"export AIRFLOW_CONN_{str(conn_id_to_transmit).upper()}='{airflow_conn_string}'"
+                )
+                logger.info(f"connection is '{airflow_conn_string}'")
+                # add cleanup of local git repo to job description
+                git_cleanup_command = f"rm -r {tmp_dir}/{workload.ti.id}"
+                logger.info(f"git cleanup is {git_cleanup_command}")
+                user_added_post_commands.append(git_cleanup_command)
 
         airflow_env_import = {"To": "airflow_config.env", "Data": env_file_content}
 
-        user_added_pre_commands.append(
-            f"source airflow_config.env && source {python_env}/bin/activate"
-        )
+        job_descr_dict["User postcommand"] = ";".join(user_added_post_commands)
         job_descr_dict["User precommand"] = ";".join(user_added_pre_commands)
         job_descr_dict["RunUserPrecommandOnLoginNode"] = (
-            "false"  # precommand includes activating the python env, this should be done on compute node right before running the job
+            "true"  # precommand needs public internet access to clone dag repos
         )
         job_descr_dict["Imports"] = [worker_script_import, airflow_env_import]
         # add user defined options to description
