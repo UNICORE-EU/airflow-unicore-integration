4b2d9e26c6dc2ff457c4c0d56cae1de4a3a0efcc
Christian Boettcher <c.boettcher@fz-juelich.de>
2026-02-04 12:35:21 +0100

    cleanup container operator

--- src/airflow_unicore_integration/operators/container.py
+++ src/airflow_unicore_integration/operators/container.py
@@ -1,6 +1,3 @@
-from typing import Dict
-from typing import List
-
 from airflow_unicore_integration.operators.unicore_operators import (
     UnicoreGenericOperator,
 )
@@ -10,13 +7,13 @@ class UnicoreContainerOperator(UnicoreGenericOperator):
     def __init__(
         self, name: str, docker_image_url: str, command: str, options: str | None = None, **kwargs
     ):
-        params: Dict[str, str | List[str]] = {"COMMAND": command, "IMAGE_URL": docker_image_url}
-        if options is not None:
-            params["OPTIONS"] = options
         super().__init__(
             name=name,
             application_name="CONTAINER",
             application_version="1.0",
-            parameters=params,
             **kwargs,
         )
+        self.params["COMMAND"] = command
+        self.params["IMAGE_URL"] = docker_image_url
+        if options is not None:
+            self.params["OPTIONS"] = options
