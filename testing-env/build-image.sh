6ddd6d712a94962cd720e83948791e04261d747c
Christian Boettcher <c.boettcher@fz-juelich.de>
2026-05-22 07:29:52 +0200

    update testing env

--- testing-env/build-image.sh
+++ testing-env/build-image.sh
@@ -5,3 +5,5 @@ PROJECT_DIR=$(dirname $0)/..
 python -m build $PROJECT_DIR
 
 docker build -t unicore-airflow -f $PROJECT_DIR/testing-env/unicore-airflow.docker $PROJECT_DIR
+
+docker build -t unicore-with-deps -f $PROJECT_DIR/testing-env/unicore-with-deps.docker $PROJECT_DIR
